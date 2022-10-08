c0e2bc4120e67b6cf93f0adbf8d49431bbe5fb35
Pranshu-Ghub25 <109868887+Pranshu-Ghub25@users.noreply.github.com>
2022-10-08 12:45:58 +0530

    Add files via upload

---
+++ FileHandlePage/SimpleFileHandlingProject.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package simplefilehandlingproject;
+
+/**
+ *
+ * @author prans
+ */
+public class SimpleFileHandlingProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        WelcomePage ob=new WelcomePage();
+        new WelcomePage().setVisible(true);
+
+    }
+    
+}
